3eb155969695d787a2e47b7550e6d7900e44068c
vinaydigarse <vinaydigarse1@gmail.com>
2022-11-09 19:10:50 +0530

    day2

---
+++ component/upper_navbar.js
@@ -0,0 +1,49 @@
+let upperNavbar = () => {
+  return `<div class="upper1">
+    <div class="vd-language-dropdown">
+      <a href="#">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/44/44386.png"
+          alt=""
+          class="globe"
+        />
+        English
+      </a>
+      <div class="vd-language-dropdown-content">
+        <a href="#">Hindi</a>
+        <a href="#">marathi</a>
+        <a href="#">urdu</a>
+        <a href="#">tamil</a>
+        <a href="#">hvdhsfcv</a>
+        <a href="#">Fkhckj</a>
+      </div>
+    </div>
+    <div class="vd-contact-sales">
+      <a href="">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1067/1067566.png"
+          alt=""
+        />
+        Contact Sales</a
+      >
+    </div>
+  </div>
+  <div class="upper2">
+          <input placeholder="Search" />
+          <a href="">Log In</a>
+          <a href="">Customer Support</a>
+          <div class="vd-about-dropdown">
+            <a href="#">About </a>
+            <div class="vd-about-dropdown-content">
+              <a href="#">About Us </a>
+              <a href="#">Careers</a>
+              <a href="#">Contact Us</a>
+              <a href="#">Investor Relations</a>
+              <a href="#">Management Team</a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+};
+
+export { upperNavbar };
