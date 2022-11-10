fa9e2cba4ab2e19f80a066f7b31605520be9e82d
priyachauhan <priya1921@gmail.com>
2022-11-10 23:43:23 +0530

    js for login and signup

---
+++ script/login.js
@@ -0,0 +1,30 @@
+let HomePage = () => {
+    window.location.href = "index.html";
+  };
+  
+  let getData = JSON.parse(localStorage.getItem("SignUp_Data"));
+  
+  let flag;
+  
+  let Login = () => {
+    event.preventDefault()
+    let email = document.getElementById("pc_email").value;
+  
+    let password = document.getElementById("pc_password").value;
+  
+    getData.forEach((el) => {
+      if (email === el.email) {
+        if (password ===el.password) {
+          flag = true;
+        }
+      } else {
+        flag = false;
+      }
+    });
+  
+    if (flag == true) {
+      window.location.href = "index.html";
+    } else {
+      alert("Please fill correct Details");
+    }
+  };
