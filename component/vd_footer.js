09277d141a2f5c08d151f185967d4f9db3bfc7e7
vinaydigarse <vinaydigarse1@gmail.com>
2022-11-11 19:40:19 +0530

    day3

---
+++ component/vd_footer.js
@@ -0,0 +1,156 @@
+let footer = () => {
+  return `<div id="vd-footer1">
+    <div class="vd-footer11">
+      <div class="vd-footer-heading"><p>Popular Features</p></div>
+      <div class="vd-footer-option1">
+        <div>
+          <a href="">Free Meeting Scheduler App</a>
+          <a href="">Social Media Tools</a>
+          <a href="">Email Tracking Software</a>
+          <a href="">Sales Email Automation</a>
+          <a href="">Ads Software</a>
+          <a href="" p>Email Marketing Software</a>
+          <a href="">Lead Management Software</a>
+          <a href="">Pipeline Management Tools</a>
+          <a href="" p>Free Website Builder</a>
+        </div>
+        <div>
+          <a href="">Sales Email Templates</a>
+          <a href="">Help Desk Software</a>
+          <a href="">Free Online Form Builder</a>
+          <a href="">Free Chatbot Builder</a>
+          <a href="">Free Live Chat Software</a>
+          <a href="">Marketing Analytics</a>
+          <a href="">Free Landing Page Builder</a>
+          <a href="">Free Web Hosting</a>
+        </div>
+      </div>
+    </div>
+    <div class="vd-footer12">
+      <div>
+        <div class="vd-footer-heading"><p>Free Tools</p></div>
+        <div class="vd-footer-option2">
+          <a href=""> Website Grader </a>
+          <a href="">Make My Personal</a>
+          <a href="">Email Signature Generator</a>
+          <a href="">Blog Ideas Generator</a>
+          <a href="">Invoice Template Generator</a>
+          <a href="">Marketing Plan Generator</a>
+          <a href="">Free Business Templates</a>
+          <a href="">Free Business Templates</a>
+          <a href="">Software Comparisons</a>
+          <a href="">Library</a>
+          <a href="">Website Themes & Templates</a>
+        </div>
+      </div>
+      <div>
+        <div class="vd-footer-heading"><p>Company</p></div>
+        <div class="vd-footer-option2">
+          <a href="">About Us</a>
+          <a href="">Careers</a>
+          <a href="">Management Team</a>
+          <a href="">Board of Diretorsr</a>
+          <a href="">Investor Reletion</a>
+          <a href="">Blog</a>
+          <a href="">Contact Us</a>
+        </div>
+      </div>
+      <div class="vd-footer-heading4">
+        <div class="vd-footer-heading">
+          <p>Customers</p>
+          <div class="vd-footer-option2">
+            <a href="">Custer Support</a>
+            <a href=""> Join a Local User Group</a>
+          </div>
+        </div>
+        <div class="vd-footer-heading">
+          <p>Partners</p>
+          <div class="vd-footer-option2">
+            <a href="">All Partner Programes</a>
+            <a href="">Solutions Partner Programe</a>
+            <a href="">App Partner Programe</a>
+            <a href=""> HubSput for SrartUp</a>
+            <a href="">Affilliate Program</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="vd-footer2">
+    <div>
+      <div>
+        <a href="#facebook">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/3128/3128304.png"
+            alt=""
+        /></a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/1384/1384063.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href=""
+          ><img
+            src="https://cdn-icons-png.flaticon.com/128/733/733579.png"
+            alt=""
+        /></a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/2111/2111499.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/2190/2190431.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/3046/3046120.png"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+  <div id="vd-footer3">
+    <div><h1>HubSpot</h1></div>
+    <div>
+      <p>Copyright © 2022 HubSpot, Inc.</p>
+    </div>
+    <div class="vd-footer33">
+      <div>
+        <a href="">Legal Stuff</a>
+      </div>
+      <div>
+        <a href="">Security</a>
+      </div>
+      <div>
+        <a href="">Privacy Policy </a>
+      </div>
+    </div>
+  </div>`;
+};
+
+export { footer };
