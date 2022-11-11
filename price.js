0febd73189bfbcb0dcfb7be562056654867eda4b
lalit_bisht <lb6995@gmail.com>
2022-11-11 08:36:41 +0530

    day3

--- price.js
+++ price.js
@@ -1,1 +1,7 @@
+let accordion=document.getElementsByClassName("contentbx");
+for(let i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+}
 
