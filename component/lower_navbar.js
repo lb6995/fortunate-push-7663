05661adc738c1820d3b8df384eefe71525ecea37
vinaydigarse <vinaydigarse1@gmail.com>
2022-11-13 09:17:04 +0530

    day5

--- component/lower_navbar.js
+++ component/lower_navbar.js
@@ -1,51 +1,192 @@
 let lowerNavbar = () => {
-  return `<div id="lower1">
-    <div><img src="" alt="logo" /></div>
-    <div class="vd-software-dropdown">
-      <a href="">Software</a>
-      <div class="vd-software-dropdown-content">
-        <a href="">a</a>
-        <a href="">b</a>
-        <a href="">c</a>
-        <a href="">d</a>
-        <a href="">e</a>
+  return ` <div id="lower1">
+  <div class="logo">
+    <img
+      src="https://knowledge.hubspot.com/hubfs/HubSpot_Logos/HSLogo_color.svg"
+      alt="logo"
+    />
+  </div>
+  <div class="vd-software-dropdown">
+    <a href="">Software</a>
+    <div class="vd-software-dropdown-content">
+      <h2>The HubSpot CRM Platform</h2>
+      <p>
+        All of HubSpot’s marketing, sales CRM, customer service, CMS,
+        and operations software on one platform.
+      </p>
+      <div class="vd-software-drapdown-option">
+        <button class="vd-software-drapdown-option-btn" href="">
+          Free HubSpot CRM
+        </button>
+        <button class="vd-software-drapdown-option-btn" href="">
+          Overview of all product
+        </button>
       </div>
-    </div>
-    <a href="">Pricing</a>
-    <div class="vd-resourse-dropdown">
-      <a href="">Resourcrs</a>
-      <div class="vd-resourse-dropdown-content">
-        <div>
-          <h2>Education</h2>
-          <a href="">Blog </a>
-          <a href="">Ebooks, Guides & More</a>
-          <a href=""> Free Courses & Certifications Inbound</a>
-          <a href="">Methodology</a>
+      <div class="vd-software-drapdown-option2">
+        <div class="vd-hub">
+          <div class="div1">
+            <img
+              src="https://www.hubspot.com/hubfs/product_icons_2022/MarketingHub_Icon_Gradient_RGB_24px.svg"
+              style="width: 100%; height: 100%"
+              alt=""
+            />
+          </div>
+          <div class="div2">
+            <h3>Marketing Hub</h3>
+          </div>
+          <div class="div3"></div>
+          <div class="div4">
+            <p>
+              Matrketing automation software.
+              <a href="">Free and premium plans</a>
+            </p> 
+          </div>
         </div>
-        <div>
-          <h2>Services</h2>
-          <a href=""> Onboarding & Consulting Services </a
-          ><a href="">Hire a Service Provider</a>
+        <div class="vd-hub">
+          <div class="div1">
+            <img
+              src="https://www.hubspot.com/hubfs/product_icons_2022/MarketingHub_Icon_Gradient_RGB_24px.svg"
+              style="width: 100%; height: 100%"
+              alt=""
+            />
+          </div>
+          <div class="div2">
+            <h3>Sales Hub</h3>
+          </div>
+          <div class="div3"></div>
+          <div class="div4">
+            <p>
+              Sales CRM software.
+              <a href="">Free and premium plans</a>
+            </p> 
+          </div>
         </div>
-        <div>
-          <h2>Why HubSpot ?</h2>
-          <a href=""> Case Studies </a
-          ><a href=""> Why Choose HubSpot? </a>
+        <div class="vd-hub">
+          <div class="div1">
+            <img
+              src="https://www.hubspot.com/hubfs/product_icons_2022/MarketingHub_Icon_Gradient_RGB_24px.svg"
+              style="width: 100%; height: 100%"
+              alt=""
+            />
+          </div>
+          <div class="div2">
+            <h3>Service Hub</h3>
+          </div>
+          <div class="div3"></div>
+          <div class="div4">
+            <p>
+              Coustomer service software.
+              <a href="">Free and premium plans</a>
+            </p> 
+          </div>
         </div>
-        <div>
-          <h2>User Resources</h2>
-          <a href="">Partner Programs</a>
-          <a href=""> Developer Tools</a>
-          <a href="">Community</a>
-          <a href=""> HubSpot Ecosystem</a>
+        <div class="vd-hub">
+          <div class="div1">
+            <img
+              src="https://www.hubspot.com/hubfs/product_icons_2022/MarketingHub_Icon_Gradient_RGB_24px.svg"
+              style="width: 100%; height: 100%"
+              alt=""
+            />
+          </div>
+          <div class="div2">
+            <h3>CMS Hub</h3>
+          </div>
+          <div class="div3"></div>
+          <div class="div4">
+            <p>
+              Content management software.
+              <a href="">Free and premium plans</a>
+            </p> 
+          </div>
         </div>
+        <div class="vd-hub">
+          <div class="div1">
+            <img
+              src="https://www.hubspot.com/hubfs/product_icons_2022/MarketingHub_Icon_Gradient_RGB_24px.svg"
+              style="width: 100%; height: 100%"
+              alt=""
+            />
+          </div>
+          <div class="div2">
+            <h3>Operatiovs Hub</h3>
+          </div>
+          <div class="div3"></div>
+          <div class="div4">
+            <p>
+             Operation software.
+              <a href="">Free and premium plans</a>
+            </p> 
+          </div>
+        </div>
+      </div>
+          <div class="vd-marketplace">
+        <h3>
+          App Marketplace
+        </h3>
+        <p>Connect your favorite apps to HubSpot. <a href="">See all integrations</a></p>
       </div>
     </div>
   </div>
-  <div id="lower2">
-    <button class="button1">Get Free CRM</button>
-    <button class="button2">Get started free</button>
-  </div>`;
+  <div><a href="">Pricing</a></div>
+  <div class="vd-resourse-dropdown">
+    <a href="">Resourcrs</a>
+    <div class="vd-resourse-dropdown-content">
+      <div>
+        <h2>
+          <img
+            src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/WBZ-1165%20Global%20Nav%20Redesign/1-icon.svg"
+            alt=""
+          />
+          Education
+        </h2>
+        <a class="vd-a-tag" href="">Blog </a>
+        <a href="">Ebooks, Guides & More</a>
+        <a href=""> Free Courses & Certifications Inbound</a>
+        <a href="">Methodology</a>
+      </div>
+      <div>
+        <h2>
+          <img
+            src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/WBZ-1165%20Global%20Nav%20Redesign/3-icon.svg"
+            alt=""
+          />
+          Services
+        </h2>
+        <a href=""> Onboarding & Consulting Services </a
+        ><a href="">Hire a Service Provider</a>
+      </div>
+      <div>
+        <h2>
+          <img
+            src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/WBZ-1165%20Global%20Nav%20Redesign/2-icon.svg"
+            alt=""
+          />
+          Why HubSpot ?
+        </h2>
+        <a href=""> Case Studies </a
+        ><a href=""> Why Choose HubSpot? </a>
+      </div>
+      <div>
+        <h2>
+          <img
+            src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/WBZ-1165%20Global%20Nav%20Redesign/4-icon.svg"
+            alt=""
+          />
+          User Resources
+        </h2>
+        <a href="">Partner Programs</a>
+        <a href=""> Developer Tools</a>
+        <a href="">Community</a>
+        <a href=""> HubSpot Ecosystem</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="lower2">
+  <button class="button1">Get Free CRM</button>
+  <button class="button2">Get started free</button>
+</div>
+</div>`;
 };
 
 export { lowerNavbar };
